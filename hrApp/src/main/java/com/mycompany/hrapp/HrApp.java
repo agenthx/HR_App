2f12424f13dbb6301b3c5d16129c8ee846505d66
husaiali <hussain.aha2005@gmail.com>
2025-05-10 18:05:00 +0300

    start

---
+++ hrApp/src/main/java/com/mycompany/hrapp/HrApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hrapp;
+
+/**
+ *
+ * @author hussa
+ */
+public class HrApp {
+
+    public static void main(String[] args) {
+        System.out.println("hello Mr.Baker!");
+    }
+}
